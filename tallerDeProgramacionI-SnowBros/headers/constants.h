666b8207f4d9849384f703592fc0bffb9346b9c8
lmasello <maselloleandro@gmail.com>
2014-08-31 17:13:48 -0700

    Cambio para ver el nro de revision. Se agrega comentario a constants.h

--- tallerDeProgramacionI-SnowBros/headers/constants.h
+++ tallerDeProgramacionI-SnowBros/headers/constants.h
@@ -14,6 +14,7 @@
 #define ANCHO_PX 640;
 #define ALTO_UN 150;
 #define ANCHO_UN 100;
+//Aqui deberia existir la funcion que obtenga el path
 #define defaultImagePath "/home/lmasello/tallerDeProgramacionWorkspace/tallerDeProgramacionI-SnowBros/resources/chapelco.jpg"; //Aca hay que agregar una funcion que obtenga automaticamente el path
 
 

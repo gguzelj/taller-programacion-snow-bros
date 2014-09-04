be7de06969b81b5ee041624d62e6e7e71fd599ee
Diego <diegofk26@gmail.com>
2014-09-04 19:34:52 -0300

    prueba commit

--- tallerDeProgramacionI-SnowBros/headers/constants.h
+++ tallerDeProgramacionI-SnowBros/headers/constants.h
@@ -15,9 +15,8 @@
 #define ALTO_UN 150;
 #define ANCHO_UN 100;
 //Aqui deberia existir la funcion que obtenga el path
-#define defaultImagePath "/home/anonimo/workspace/git/taller-programacion-snow-bros/tallerDeProgramacionI-SnowBros/resources/chapelco.jpg"; //Aca hay que agregar una funcion que obtenga automaticamente el path
+#define defaultImagePath "/home/lmasello/tallerDeProgramacionWorkspace/tallerDeProgramacionI-SnowBros/resources/ImagenGrande.jpg"; //Aca hay que agregar una funcion que obtenga automaticamente el path
 //#define defaultImagePath "/home/fdc/git/taller-programacion-snow-bros/tallerDeProgramacionI-SnowBros/resources/ImagenGrande.jpg";
-
 
 #define TECLA_RESET 114
 
--- tallerDeProgramacionI-SnowBros/src/JsonParser.cpp
+++ tallerDeProgramacionI-SnowBros/src/JsonParser.cpp
@@ -1,3 +1,4 @@
+
 /*
  * JsonParser.cpp
  *

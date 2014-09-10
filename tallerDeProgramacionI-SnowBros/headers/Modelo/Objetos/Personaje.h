831c9529465e548db9af3d635c3e85994e78907e
Fernando Cancellara <fdcancellara@gmail.com>
2014-09-10 16:17:23 -0300

    Modificado Personaje.h Personaje.cpp

--- tallerDeProgramacionI-SnowBros/headers/Modelo/Objetos/Personaje.h
+++ tallerDeProgramacionI-SnowBros/headers/Modelo/Objetos/Personaje.h
@@ -4,17 +4,28 @@
 #include "../Objetos/Figuras/Figura.h"
 
 class Personaje : public Figura{
-	private:
-		b2Vec2 posicion;
-		b2World* world; 			//Viene por parametro.
-		b2Body* cuerpoDinamicoPersonaje;
-		int movimientoIzquierda, movimientoDerecha, movimientoNada, velocidadLimite, aceleracion, frenado;
-	public:
-		Personaje(JsonParser *parser, b2World* world);
-		~Personaje();	//Destructor del personaje
-		void Moverse(int); 			//Alguna clase controlador o algo, hasta tal vez un int segun que tipo de movimiento se eligio
-		void Saltar();
-		b2Vec2 Posicion();
+        private:
+                b2Vec2 posicion;
+                b2World* world;                         //Viene por parametro.
+                b2Body* cuerpoDinamicoPersonaje;
+                int movimientoIzquierda, movimientoDerecha, movimientoNada, velocidadLimite, aceleracion, frenado;
+                void Saltar(b2Vec2 VelocidadActual);
+                int stickiness;                 //Para mostrar un poco de inercia
+                int duracionStickiness;
+        enum movimientos {
+            Mov_Nada,
+            Mov_Izquierda,
+            Mov_Derecha,
+        };
+                movimientos movimientoActual;
+                movimientos movimientoAnterior; //Va a servir para no interrumpir el movimiento al momento del salto
+
+        public:
+                Personaje(JsonParser *parser, b2World* world);
+                ~Personaje();   //Destructor del personaje
+                void Moverse(); //Alguna clase controlador o algo, hasta tal vez un int segun que tipo de movimiento se eligio
+                b2Vec2 Posicion();
+                void Keyboard(unsigned char key); //Deberia estar aca o en otro lado?
 };
 
 
--- tallerDeProgramacionI-SnowBros/src/Modelo/Objetos/Personaje.cpp
+++ tallerDeProgramacionI-SnowBros/src/Modelo/Objetos/Personaje.cpp
@@ -2,61 +2,93 @@
 
 Personaje::Personaje(JsonParser *parser, b2World* world){
 
-	this->world = new b2World(b2Vec2(0, -9.8f)); //Nota: esto no se crea aca, se pasa por parametro. Lo dejo por ahora para que no tire warning
+        this->stickiness = 0; //Lo inicializo en 0
+        this->world = new b2World(b2Vec2(0, -9.8f)); //Nota: esto no se crea aca, se pasa por parametro. Lo dejo por ahora para que no tire warning
+        this->velocidadLimite = 15.0f;
+        this->aceleracion = 7.4f;
+        this->frenado = 0.97;
+        this->duracionStickiness = 25;
 
-	this->movimientoIzquierda = -1;
-	this->movimientoDerecha = 1;
-	this->movimientoNada = 0;
-	this->velocidadLimite = 20;
-	this->aceleracion = 10;
-	this->frenado = -10;
-	//definiendo el body del personaje
-	b2BodyDef cuerpoDelPersonaje;
-	cuerpoDelPersonaje.type = b2_dynamicBody; 			//El personaje sera un cuerpo dinamico
-	cuerpoDelPersonaje.angle = 0; 						//Angulo 0 asi empieza recto
-	cuerpoDelPersonaje.position.Set(0,0); 				//donde queremos que empiece
+        //definiendo el body del personaje
+        b2BodyDef cuerpoDelPersonaje;
+        cuerpoDelPersonaje.type = b2_dynamicBody;                       //El personaje sera un cuerpo dinamico
+        cuerpoDelPersonaje.angle = 0;                                           //Angulo 0 asi empieza recto
+        cuerpoDelPersonaje.position.Set(0,0);                           //donde queremos que empiece
 
-	b2Body* cuerpoDinamicoDelPersonaje = this->world->CreateBody(&cuerpoDelPersonaje);
-	this->cuerpoDinamicoPersonaje = cuerpoDinamicoDelPersonaje;
+        b2Body* cuerpoDinamicoDelPersonaje = this->world->CreateBody(&cuerpoDelPersonaje);
+        this->cuerpoDinamicoPersonaje = cuerpoDinamicoDelPersonaje;
 
-	//definiendo ahora el fixture del personaje
-	b2PolygonShape formaDelPersonaje;					//esto va a ser si queremos que sea una caja...
-	formaDelPersonaje.SetAsBox(1,1); 					//...con las siguientes dimensiones
-	b2FixtureDef fixtureDelPersonaje;					//creo el fixture
-	fixtureDelPersonaje.shape = &formaDelPersonaje;		//le asigno la forma que determine antes
-	fixtureDelPersonaje.density = 1;					//una densidad cualquiera
-	cuerpoDinamicoDelPersonaje->CreateFixture(&fixtureDelPersonaje);
+        //definiendo ahora el fixture del personaje
+        b2PolygonShape formaDelPersonaje;                                       //esto va a ser si queremos que sea una caja...
+        formaDelPersonaje.SetAsBox(1,1);                                        //...con las siguientes dimensiones
+        b2FixtureDef fixtureDelPersonaje;                                       //creo el fixture
+        fixtureDelPersonaje.shape = &formaDelPersonaje;         //le asigno la forma que determine antes
+        fixtureDelPersonaje.density = 1;                                        //una densidad cualquiera
+        cuerpoDinamicoDelPersonaje->CreateFixture(&fixtureDelPersonaje);
 
 }
 Personaje::~Personaje(){
-	//una posible destruccion del personaje
-	this->world->DestroyBody(this->cuerpoDinamicoPersonaje);
-	//no se si me estare olvidando de algun destructor
+        //una posible destruccion del personaje
+        this->world->DestroyBody(this->cuerpoDinamicoPersonaje);
+        //no se si me estare olvidando de algun destructor
 }
 
-void Personaje::Moverse(int numeroIngresado){
-	//Movimiento con aceleracion, podriamos hacer que si deja de tocar una tecla se empiece a frenar bruscamente
-	float apliqueDeEmpuje;
-	b2Vec2 velocidadActual = this->cuerpoDinamicoPersonaje->GetLinearVelocity(); //va a servir para cambiarla
-	switch (numeroIngresado)
-	{
-	case -1: //movimiento a izquierda
-		if (velocidadActual.x > -(this->velocidadLimite))
-		apliqueDeEmpuje = -(this->aceleracion);
-	break;
+void Personaje::Moverse(){
+        b2Vec2 velocidadActual = this->cuerpoDinamicoPersonaje->GetLinearVelocity(); //va a servir para cambiarla
+        switch (this->movimientoActual)
+                {
+                case Mov_Izquierda:
+                        velocidadActual.x = b2Max( (velocidadActual.x)-(this->aceleracion), (this->velocidadLimite)*-1.0f ); //Le fijo una aceleracion y velocidad maxima
+                break;
 
-	case 1:	//movimiento a derecha
-		if (velocidadActual.x < this->velocidadLimite)
-			apliqueDeEmpuje = this->aceleracion;
-		break;
+                case Mov_Derecha:
+                    velocidadActual.x = b2Min( (velocidadActual.x)+(this->aceleracion), (this->velocidadLimite)*1.0f ); //Le fijo una aceleracion y velocidad maxima
+                        break;
 
-	case 0: //no se apreto boton
-		apliqueDeEmpuje = velocidadActual.x * (this->frenado);
-		break;
-	}
-	this->cuerpoDinamicoPersonaje->ApplyForce(b2Vec2(apliqueDeEmpuje,0), this->cuerpoDinamicoPersonaje->GetWorldCenter(),true); //tuve que agregar el true porque cambiaron el applyforce, no se que pasa si dejo false
+                case Mov_Nada: //no se apreto boton
+                        velocidadActual.x = velocidadActual.x * (this->frenado);
+                        break;
+                }
+        this->cuerpoDinamicoPersonaje->SetLinearVelocity( velocidadActual );    //Finalmente es aplicada
+
+    if (velocidadActual.y<=0.01 && velocidadActual.y >= -0.01 && stickiness > this->duracionStickiness){
+        stickiness = 0;
+        this->movimientoActual = Mov_Nada;
+    }
+        stickiness++;
+    //ESTO ULTIMO DEBERIA ESTAR ADENTRO DEL STEP
 }
 
-void Personaje::Saltar(){
+void Personaje::Keyboard(unsigned char key)
+{
+        switch (key)
+        {
+          case 'a': //Deberia ser cambiado a flecha izquierda
+                this->movimientoActual = Mov_Izquierda;
+                this->movimientoAnterior = Mov_Izquierda;
+                break;
 
+          case 'd': //Deberia ser cambiado a flecha derecha
+                this->movimientoActual = Mov_Derecha;
+                this->movimientoAnterior = Mov_Derecha;
+                break;
+
+          case 'w': //Deberia ser cambiado a flecha arriba
+                this->Saltar(this->cuerpoDinamicoPersonaje->GetLinearVelocity());
+                        break;
+          default: //stop
+                  this->movimientoActual = Mov_Nada;
+                break;
+        }
 }
+
+void Personaje::Saltar(b2Vec2 velocidadActual){
+          if(velocidadActual.y < 0.01f && velocidadActual.y > -0.01f)   //Voy a ver que este quieto para saltar, si uso velocidad.y==0 tengo problemas
+          {
+                float potenciaDeSalto = this->cuerpoDinamicoPersonaje->GetMass() * 7;
+                this->cuerpoDinamicoPersonaje->ApplyLinearImpulse( b2Vec2(0,potenciaDeSalto), this->cuerpoDinamicoPersonaje->GetWorldCenter(),true );
+          };
+          this->movimientoActual = this->movimientoAnterior;    //Para que el salto no interrumpa el movimiento que tenia de antes
+}
+
+

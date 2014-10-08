bcbca7366ee841c59bef159319d2595f7d1e6305
lmasello <maselloleandro@gmail.com>
2014-10-08 11:25:28 -0300

    * Automatizo la coordenada del msj * Automatizo la cantidad de px del SDL_Rect "dst" sobre el cual se dibuja la pantalla

--- tallerDeProgramacionI-SnowBros/headers/Vista/Drawer.h
+++ tallerDeProgramacionI-SnowBros/headers/Vista/Drawer.h
@@ -78,6 +78,30 @@ private:
 	void manageSDL_ttfError();
 	void manageSDL_ttfLoadFontError();
 	void manageDrawMessagesError();
+
+	/**
+	* Draw an SDL_Texture to an SDL_Renderer at some destination rect
+	* taking a clip of the texture if desired
+	* @param tex The source texture we want to draw
+	* @param ren The renderer we want to draw to
+	* @param dst The destination rectangle to render the texture to
+	* @param clip The sub-section of the texture to draw (clipping rect)
+	*		default of nullptr draws the entire texture
+	*/
+	void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst,SDL_Rect *clip);
+	/**
+	* Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
+	* the texture's width and height and taking a clip of the texture if desired
+	* If a clip is passed, the clip's width and height will be used instead of
+	*	the texture's
+	* @param tex The source texture we want to draw
+	* @param ren The renderer we want to draw to
+	* @param x The x coordinate to draw to
+	* @param y The y coordinate to draw to
+	* @param clip The sub-section of the texture to draw (clipping rect)
+	*		default of nullptr draws the entire texture
+	*/
+	void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, SDL_Rect *clip);
 };
 
 
--- tallerDeProgramacionI-SnowBros/src/Vista/Drawer.cpp
+++ tallerDeProgramacionI-SnowBros/src/Vista/Drawer.cpp
@@ -15,42 +15,17 @@
 #define COTA_SUP_X	(ancho_px)/5
 #define COTA_SUP_Y	(alto_px)/5
 
-
-/**
-* Draw an SDL_Texture to an SDL_Renderer at some destination rect
-* taking a clip of the texture if desired
-* @param tex The source texture we want to draw
-* @param ren The renderer we want to draw to
-* @param dst The destination rectangle to render the texture to
-* @param clip The sub-section of the texture to draw (clipping rect)
-*		default of nullptr draws the entire texture
-*/
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst,
-	SDL_Rect *clip = nullptr)
-{
+void Drawer::renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst,SDL_Rect *clip = nullptr){
 	SDL_RenderCopy(ren, tex, clip, &dst);
 }
-/**
-* Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
-* the texture's width and height and taking a clip of the texture if desired
-* If a clip is passed, the clip's width and height will be used instead of
-*	the texture's
-* @param tex The source texture we want to draw
-* @param ren The renderer we want to draw to
-* @param x The x coordinate to draw to
-* @param y The y coordinate to draw to
-* @param clip The sub-section of the texture to draw (clipping rect)
-*		default of nullptr draws the entire texture
-*/
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y,
-	SDL_Rect *clip = nullptr)
-{
+
+void Drawer::renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, SDL_Rect *clip = nullptr){
 	SDL_Rect dst;
 	dst.x = x;
 	dst.y = y;
 	if (clip != nullptr){
-		dst.w = 800;
-		dst.h = 600;
+		dst.w = ancho_px;
+		dst.h = alto_px;
 	}
 	else {
 		SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
@@ -128,8 +103,8 @@ void Drawer::drawMessages(){
 		manageDrawMessagesError();
 
 	//Set the coordinates which we want to draw to
-	float coordXDelMensaje = ancho_px/2 - 1 * un_to_px_x_inicial;
-	float coordYDelMensaje = alto_px/2 - 8 * un_to_px_y_inicial;
+	float coordXDelMensaje = ancho_px/2 - 1 * un_to_px_x_inicial; //Centro en x
+	float coordYDelMensaje = 1 * un_to_px_y_inicial; //Parte superior de la pantalla
 
 	//Render the first message
 	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer,messageAboutPoints);

5ce6ed9e8466cd680c76a7a25d71b67d0b2054a5
German Guzelj <german.guzelj@gmail.com>
2014-10-20 16:05:37 -0300

    No estoy muy seguro ed los cambios en el server, pero estoy tratando e que se dibujen los objetos!

--- tallerDeProgramacionI-SnowBros/src/Server.cpp
+++ tallerDeProgramacionI-SnowBros/src/Server.cpp
@@ -366,21 +366,7 @@ void Server::prepararEnvio() {
 	dataToBeSent.personajes = model_->getPersonajesParaEnvio();
 	dataToBeSent.dinamicos = model_->getObjetosDinamicos();
 
-	std::cout << std::endl << "Estos son los objetos Dinamicos que mandamos"
-			<< std::endl;
-	for (unsigned int i = 0; i < model_->getCantObjDinamicos(); i++) {
-		std::cout << "tipo: " << dataToBeSent.dinamicos[i].id << std::endl;
-		std::cout << "alto: " << dataToBeSent.dinamicos[i].alto << std::endl;
-		std::cout << "ancho: " << dataToBeSent.dinamicos[i].ancho << std::endl;
-		std::cout << "rotacion: " << dataToBeSent.dinamicos[i].rotacion
-				<< std::endl;
-		std::cout << "centrox: " << dataToBeSent.dinamicos[i].centro.x
-				<< std::endl;
-		std::cout << "centroy: " << dataToBeSent.dinamicos[i].centro.y
-				<< std::endl << std::endl;
-	}
-
-// Inserta el dato a mandar en la cola de envios de cada thread
+	// Inserta el dato a mandar en la cola de envios de cada thread
 	for (unsigned int i = 0; i < connections_.size(); i++) {
 
 		//Ignoramos las conexiones que no estan activas

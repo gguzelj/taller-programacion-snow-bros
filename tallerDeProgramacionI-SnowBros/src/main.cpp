2f9a385b7cf8277410f9511420e7b2c3859589ac
lmasello <maselloleandro@gmail.com>
2014-10-19 23:26:44 -0300

    Agrego indicacion para correr el ejecutable

--- tallerDeProgramacionI-SnowBros/src/main.cpp
+++ tallerDeProgramacionI-SnowBros/src/main.cpp
@@ -1,6 +1,10 @@
 #include "../headers/Client.h"
 #include <iostream>
 
+// Para correr el exe, pasarle los siguientes parametros:
+// host: Representa la ip del server o bien localhost si se corre en la misma maquina
+// port: EL puerto donde esta escuchando el server
+// username: ID del usuario
 int main(int argc, char* argv[]) {
 
 	Client* client = new Client();

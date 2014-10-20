b368ed5ba772b35c002b1176ced54e83769c62d7
lmasello <maselloleandro@gmail.com>
2014-10-19 23:31:41 -0300

    Agrego comentario para correr el ejecutable

--- tallerDeProgramacionI-SnowBros/src/main.cpp
+++ tallerDeProgramacionI-SnowBros/src/main.cpp
@@ -2,6 +2,9 @@
 #include "../headers/Server.h"
 #include <thread>
 
+// Para correr el ejecutable, pasarle los siguientes parametros:
+// jsonPath: Configuracion del juego
+// port: Puerto en el cual escucha el cliente
 int main(int argc, char *argv[]) {
 
 	Server *server = new Server();

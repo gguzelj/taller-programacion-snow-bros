61d21a01e0d73a2245c3d6c6f93c99fb0c26b597
lmasello <maselloleandro@gmail.com>
2014-11-18 15:34:57 -0300

    Mejoro el movimiento del enemigo fuego

--- tallerDeProgramacionI-SnowBros/src/Modelo/Objetos/Personajes/EnemigoFuego.cpp
+++ tallerDeProgramacionI-SnowBros/src/Modelo/Objetos/Personajes/EnemigoFuego.cpp
@@ -39,17 +39,44 @@ void EnemigoFuego::mover() {
 	if (movimientoDisparar)
 		movimientoDisparar = false;
 
+	int i = 0;
+	std::list<Personaje*>* personajes_ = escenario_->getPersonajes();
+
+	float posicionesX[4];
+	float posicionesY[4];
+	for (auto personaje = personajes_->begin(); personaje != personajes_->end(); ++personaje) {
+		posicionesX[i] = (*personaje)->getX();
+		posicionesY[i] = (*personaje)->getY();
+		i++;
+	}
+	float posicionPersonajeX = posicionesX[0];
+	float posicionPersonajeY = posicionesY[0];
+	float posicionEnemigoX = this->getX();
+	float posicionEnemigoY = this->getY();
+
 	int v1 = rand() % 100;
-
-	if (v1 <= 20)
-		this->handleInput(SDLK_LEFT, SDL_KEYDOWN);
-	else if (v1 <= 40)
-		this->handleInput(SDLK_RIGHT, SDL_KEYDOWN);
-	else if (v1 <= 60)
-		this->handleInput(SDLK_LEFT, SDL_KEYUP);
-	else if (v1 <= 80)
-		this->handleInput(SDLK_RIGHT, SDL_KEYUP);
-	else if (v1 <= 82)
-		this->handleInput(SDLK_SPACE, SDL_KEYDOWN);
-
+	if (v1 < 45) {
+		v1 = rand() % 100;
+		if (v1 <= 25)
+			this->handleInput(SDLK_LEFT, SDL_KEYDOWN);
+		else {
+			if (v1 <= 50)
+				this->handleInput(SDLK_RIGHT, SDL_KEYDOWN);
+			else {
+				if (v1 <= 75)
+					this->handleInput(SDLK_RIGHT, SDL_KEYUP);
+				else
+					this->handleInput(SDLK_LEFT, SDL_KEYUP);
+			}
+		}
+	} else if (v1 < 50) {
+		if (posicionPersonajeX < posicionEnemigoX) {
+			this->handleInput(SDLK_RIGHT, SDL_KEYUP);
+			this->handleInput(SDLK_LEFT, SDL_KEYDOWN);
+		}
+		if (posicionPersonajeX > posicionEnemigoX) {
+			this->handleInput(SDLK_LEFT, SDL_KEYUP);
+			this->handleInput(SDLK_RIGHT, SDL_KEYDOWN);
+		}
+	}
 }
